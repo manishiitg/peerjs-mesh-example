c09c594a72eec798970071f81f960416fa8ed28b
jitsi <jitsi@gmail.com>
2021-05-10 06:56:39 +0200

    demo

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import PeerClient from "./PeerClient"
+import React from 'react';
+import { Call } from "@manishiitg/webrtc-call"
 
 function App() {
   return (
     <div className="App">
-      <PeerClient />
+      <Call />
     </div>
   );
 }
